0f98b64de0d3c325c58121d6bf5c52efe7886986
Raoul Millais <raoul@raoulmillais.com>
2013-02-17 13:59:42 +0000

    Value escaping, flowids and timestamps

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 var util = require('util');
 
 module.exports = {
+	escapeValue: function escapeValue(value) {
+		return value.replace('|', '||').replace('\'', '|\'')
+			.replace('\n', '|n').replace('\r', '|r')
+			.replace('[', '|[').replace(']', '|]');
+	},
 	formatMessageProperties: function formatMessageProperties(properties) {
+		var self = this;
 		var formatted = '';
 		Object.keys(properties).forEach(function (key) {
-			formatted += util.format(' %s=\'%s\'', key, properties[key]);
+			formatted += util.format(' %s=\'%s\'', key,
+				self.escapeValue(properties[key]));
 		});
 		return formatted;
 	},
-	formatMessage: function formatMessage(name, valueOrProperties) {
-		if (typeof valueOrProperties === 'string') {
-			return util.format('##teamcity[%s \'%s\']', name, valueOrProperties);
-		} else {
-			return util.format('##teamcity[%s%s]',
-				name, this.formatMessageProperties(valueOrProperties));
-		}
+	formatMessage: function formatMessage(name, valueOrProperties,
+										flowId, disableTimestamp) {
+		var timestamp = !!disableTimestamp ? '' :
+			util.format(' timestamp=\'%s\'', new Date().toISOString());
+		flowId = flowId ? util.format(' flowId=\'%s\'', flowId) : '';
+		valueOrProperties = (typeof valueOrProperties === 'string') ?
+			util.format(' \'%s\'', this.escapeValue(valueOrProperties)) :
+			this.formatMessageProperties(valueOrProperties);
+
+		return util.format('##teamcity[%s%s%s%s]',
+			name, valueOrProperties, flowId, timestamp);
 	}
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,16 +3,75 @@ var teamcity = require('../');
 
 describe('Teamcity Service Messages', function () {
 
-	it('should format single attribute messages', function () {
-		var message = teamcity.formatMessage('messageName', 'value');
-		assert.equal(message, '##teamcity[messageName \'value\']');
+	describe('formatMessage', function () {
+
+		it('should format single attribute messages', function () {
+			var message = teamcity.formatMessage('messageName', 'value',
+				undefined, true);
+			assert.equal(message, '##teamcity[messageName \'value\']');
+		});
+
+		it('should format multiple attribute messages', function () {
+			var message = teamcity.formatMessage('messageName', {
+				name1: 'value1',
+				name2: 'value2'
+			}, undefined, true);
+			assert.equal(message, '##teamcity[messageName name1=\'value1\' name2=\'value2\']');
+		});
+
+		it('should escape values', function () {
+			var message = teamcity.formatMessage('messageName', {
+				name1: 'value1',
+				name2: 'value2|'
+			}, undefined, true);
+			assert.equal(message, '##teamcity[messageName name1=\'value1\' name2=\'value2||\']');
+		});
+
+		it('should escape simple message values', function () {
+			var message = teamcity.formatMessage('messageName', 'value2|',
+				undefined, true);
+			assert.equal(message, '##teamcity[messageName \'value2||\']');
+		});
+
+		it('should add a flowid to messages', function () {
+			var message = teamcity.formatMessage('messageName', 'value',
+				'flow1', true);
+			assert.equal(message, '##teamcity[messageName \'value\' flowId=\'flow1\']');
+		});
+
+		it('should add a timestamp to messages by default', function () {
+			var message = teamcity.formatMessage('messageName', 'value');
+			assert.ok(/##teamcity\[messageName 'value' timestamp='\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z'\]/.test(message));
+		});
 	});
 
-	it('should format multiple attribute messages', function () {
-		var message = teamcity.formatMessage('messageName', {
-			name1: 'value1',
-			name2: 'value2'
+	describe('escapeValue', function () {
+
+		it('should escape apostrophes', function () {
+			var escaped = teamcity.escapeValue('\'');
+			assert.equal(escaped, '|\'');
 		});
-		assert.equal(message, '##teamcity[messageName name1=\'value1\' name2=\'value2\']');
+
+		it('should escape newlines', function () {
+			var escaped = teamcity.escapeValue('\n');
+			assert.equal(escaped, '|n');
+		});
+
+		it('should escape carriage returns', function () {
+			var escaped = teamcity.escapeValue('\r');
+			assert.equal(escaped, '|r');
+		});
+
+		it('should escape pipes', function () {
+			var escaped = teamcity.escapeValue('\n|');
+			assert.equal(escaped, '|n||');
+		});
+
+		it('should escape square brackets', function () {
+			var escaped = teamcity.escapeValue('[]');
+			assert.equal(escaped, '|[|]');
+		});
+
 	});
+
 });
